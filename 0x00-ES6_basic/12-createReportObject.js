2d0dd8bec8a7ddebd21f4a8ee39ea5e2488faf8e
Antoniomary <amanso.totti@yahoo.com>
2024-08-05 16:25:02 +0000

    12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const ret = {
+    allEmployees: {...employeesList,},
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    }
+  };
+
+  return ret;
+}
