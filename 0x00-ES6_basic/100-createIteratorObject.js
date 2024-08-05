cb440feb01512c995b368cfe653d8514bab04298
Antoniomary <amanso.totti@yahoo.com>
2024-08-05 16:59:09 +0000

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,8 +2,10 @@ export default function createIteratorObject(report) {
   const employeesList = [];
 
   for (const department in report.allEmployees) {
-    employeesList.push(...report.allEmployees[department]);
-  };
+    if (department) {
+      employeesList.push(...report.allEmployees[department]);
+    }
+  }
 
   return employeesList;
 }
