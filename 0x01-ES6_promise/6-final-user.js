a0361b936876437073e133eeee731e8955c04ab9
Antoniomary <amanso.totti@yahoo.com>
2024-08-06 19:37:57 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,6 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignUp(firstName, lastName, fileName) {
   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => [{ status: 'resolved', value: response }])
-    .catch((error) => [{ status: 'rejected', value: error }]);
+    .then((response) => [{ status: response.status, value: response }])
+    .catch((error) => [{ status: error.status, value: error }]);
 }
