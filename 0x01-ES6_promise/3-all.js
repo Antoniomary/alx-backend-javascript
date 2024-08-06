ad25f63fe2fe2d0b3a3b4ca8d4824434cc1ce406
Antoniomary <amanso.totti@yahoo.com>
2024-08-06 18:49:49 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((response) => console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`))
+    .then(([resp1, resp2]) => console.log(`${resp1.body} ${resp2.firstName} ${resp2.lastName}`))
     .catch(() => console.error('Signup system offline'));
 }
