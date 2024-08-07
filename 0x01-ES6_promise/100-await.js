8c6f1a6b3be43a92cb9f1120d8d4ea5dfbeeee2d
Antoniomary <amanso.totti@yahoo.com>
2024-08-07 08:32:38 +0000

    100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-
-  if (photo && user) {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
     return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
   }
-  return { photo: null, user: null };
 }
