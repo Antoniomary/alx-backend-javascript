3070d29676be1a550cf75da4231c03523080a721
Antoniomary <amanso.totti@yahoo.com>
2024-08-14 01:56:49 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(set, startString) {
   if (!startString) return '';
+  if (set instanceof Set === false) return '';
   return Array.from(set)
     .filter((each) => each.startsWith(startString))
     .map((each) => each.slice(startString.length))
