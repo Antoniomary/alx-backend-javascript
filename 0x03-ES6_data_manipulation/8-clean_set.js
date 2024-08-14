427bf200a43072354a39c2d5366bf22619024156
Antoniomary <amanso.totti@yahoo.com>
2024-08-14 02:11:48 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   if (!startString) return '';
   return Array.from(set)
-    .filter((each) => each.startsWith(startString) && startString.length > each.length)
+    .filter((each) => each.startsWith(startString))
     .map((each) => each.slice(startString.length))
     .join('-');
 }
