bdebe695d98f315d3f7f9e73a146ad2ffa0b1653
Antoniomary <amanso.totti@yahoo.com>
2024-08-14 03:51:14 +0000

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,8 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const counter = weakMap.get(endpoint);
-  if (counter === undefined) weakMap.set(endpoint, 1);
-  else if (counter + 1 >= 5) {
-    throw new Error('Endpoint load is high');
-  } else {
-    weakMap.set(endpoint, counter + 1);
-  }
+  const count = weakMap.get(endpoint);
+  if (count === undefined) weakMap.set(endpoint, 1);
+  else weakMap.set(endpoint, count + 1);
+  if (count + 1 >= 5) throw new Error('Endpoint load is high');
 }
