88e45fe3fdc04d6f4906690f77396b918634c127
Antoniomary <amanso.totti@yahoo.com>
2024-08-14 03:32:31 +0000

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const counter = weakMap.get(endpoint);
   if (counter === undefined) weakMap.set(endpoint, 1);
-  else if (counter >= 5) {
+  else if (counter + 1 >= 5) {
     throw new Error('Endpoint load is high');
   } else {
     weakMap.set(endpoint, counter + 1);
