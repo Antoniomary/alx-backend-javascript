136cedbe7e116198753543e88af86b7b3a2e08f8
Antoniomary <amanso.totti@yahoo.com>
2024-10-05 14:17:14 +0000

    used before and after Each

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,15 +5,23 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('verify calculateNumber usage', () => {
-    const myStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    const mySpy = sinon.spy(console, 'log');
+  let myStub;
+  let mySpy;
 
+  beforeEach(() => {
+    myStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    mySpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    myStub.restore();
+    mySpy.restore();
+  });
+
+  it('verify calculateNumber usage', () => {
     sendPaymentRequestToApi(100, 20);
 
     expect(myStub.calledWith('SUM', 100, 20)).to.be.true;
     expect(mySpy.calledWith('The total is: 10')).to.be.true;
-
-    mySpy.restore();
   });
 });
