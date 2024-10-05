affe37166326f0ececa13945b54a17f47be11a0f
Antoniomary <amanso.totti@yahoo.com>
2024-10-05 12:55:09 +0000

    utils.js

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      b = Math.round(b);
+      if (b === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / b;
+    }
+  }
+};
+
+module.exports = Utils;
