06baa4d2c88a67e38140ae22382951fd860d2e30
Antoniomary <amanso.totti@yahoo.com>
2024-10-21 22:47:06 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,23 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (input) => {
-  const name = String(input).trim();
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (input) => {
+    const name = input.toString().trim();
 
-  console.log(`Your name is: ${name}`);
+    console.log(`Your name is: ${name}`);
 
-  if (process.stdout.isTTY && !process.stdin.isTTY) {
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (input) => {
+    const name = input.toString().trim();
+
+    console.log(`Your name is: ${name}`);
+
+    process.exit();
+  });
+
+  process.on('exit', () => {
     console.log('This important software is now closing');
-  }
-
-  process.exit();
-});
+  });
+}
