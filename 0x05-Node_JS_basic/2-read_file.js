a5fa3d84b227595fda12f63ca98b1cc591bae301
Antoniomary <amanso.totti@yahoo.com>
2024-10-22 00:17:14 +0000

    2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,11 +1,36 @@
+const { readFileSync } = require('fs');
+
 function countStudents(path) {
   try {
-    readFile(path);
+    const data = readFileSync(path, 'utf-8');
+
+    const lines = data.trim().split('\n').slice(1);
+
+    let count = 0;
+
+    const students = {};
+    lines.forEach((line) => {
+      const words = line.split(',');
+      const firstName = words[0];
+      const field = words[3];
+
+      if (!students[field]) students[field] = [];
+      students[field].push(firstName);
+
+      count += 1;
+    });
+
     console.log(`Number of students: ${count}`);
+
+    for (const [field, names] of Object.entries(students)) {
+      const n = names.length;
+      const all = names.join(', ');
+
+      console.log(`Number of students in ${field}: ${n}. List: ${all}`);
+    }
   } catch (error) {
     throw Error('Cannot load the database');
   }
-Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
 }
 
 module.exports = countStudents;
