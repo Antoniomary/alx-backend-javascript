6eb36c44c31a4b54fd94a622a3e7c57ac96bfdac
Antoniomary <amanso.totti@yahoo.com>
2024-10-22 11:06:19 +0000

    7-http_express.js

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const { readFile } = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf-8', (err, data) => {
+      if (err) reject(new Error('Cannot load the database'));
+      else {
+        const lines = data.trim().split('\n').slice(1);
+
+        let count = 0;
+
+        const students = {};
+        lines.forEach((line) => {
+          const words = line.split(',');
+          const firstName = words[0];
+          const field = words[3];
+
+          if (!students[field]) students[field] = [];
+          students[field].push(firstName);
+
+          count += 1;
+        });
+
+        let result = `Number of students: ${count}\n`;
+
+        for (const [field, names] of Object.entries(students)) {
+          const n = names.length;
+          const all = names.join(', ');
+
+          result += `Number of students in ${field}: ${n}. List: ${all}\n`;
+        }
+
+        resolve(result);
+      }
+    });
+  });
+}
+
+const port = 1245;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  let result = 'This is the list of our students\n';
+  countStudents(process.argv[2])
+    .then((data) => {
+      result += data;
+      res.end(result.slice(0, -1));
+    }).catch((err) => {
+      res.stastusCode = 404;
+      res.end(result + err.message);
+    });
+});
+
+app.listen(port, () => {});
+
+module.exports = app;
