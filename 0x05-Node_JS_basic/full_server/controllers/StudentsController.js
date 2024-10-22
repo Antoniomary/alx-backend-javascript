0cc9b25ac3057e55da1485bf87946cd282f1b77c
Antoniomary <amanso.totti@yahoo.com>
2024-10-22 20:38:18 +0000

    changed parameter names to full

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,49 +1,33 @@
 import readDatabase from '../utils';
 
 class StudentsController {
-  static getAllStudents(req, res) {
-    const dbPath = req.databasePath || './database.csv';
-    let result = '';
-
-    readDatabase(dbPath)
+  static getAllStudents(request, response) {
+    readDatabase(process.argv[2])
       .then((students) => {
-        result = 'This is the list of our students\n';
+        let result = 'This is the list of our students\n';
 
         for (const [field, names] of Object.entries(students)) {
-          const sorted = names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-
           const n = names.length;
-          const all = sorted.join(', ');
+          const all = names.join(', ');
           result += `Number of students in ${field}: ${n}. List: ${all}\n`;
         }
-        return res.send(result);
-      }).catch((err) => {
-        res.statusCode = 500;
-        return res.send(err.message);
-      });
+
+        return response.status(200).send(result.trim());
+      }).catch((err) => response.status(500).send(err.message));
   }
 
-  static getAllStudentsByMajor(req, res) {
-    const { major } = req.params;
-    const dbPath = req.databasePath || './database.csv';
-    let result = '';
-
+  static getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
     if (!major || !['CS', 'SWE'].includes(major)) {
-      return res.status(500).send('Major parameter must be CS or SWE');
+      return response.status(500).send('Major parameter must be CS or SWE');
     }
 
-    readDatabase(dbPath)
+    readDatabase(process.argv[2])
       .then((students) => {
         const names = students[major];
-        const sorted = names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-
-        result += `List: ${sorted.join(', ')}\n`;
-      }).catch((err) => {
-        res.statusCode = 500;
-        result += err.message;
-      });
-
-    return res.send(result);
+        const result = `List: ${names.join(', ')}`;
+        return response.send(result);
+      }).catch((err) => response.status(500).send(err.message));
   }
 }
 
